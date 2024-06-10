9003c996432a949b2c63f7b61d2926afaaaaacae
Natum Tongiyang <95202148+Smuggin@users.noreply.github.com>
2024-06-10 22:47:27 +0700

    fix: Update nextJS config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+    async rewrites() {
+      return [
+        {
+          source: "/__/auth/:path*",
+          destination: `https://evedenmetacom.firebaseapp.com/__/auth/:path*`,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
